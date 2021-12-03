956bbaa694b7f6025a550c75f6b0221fb699cd09
Pedro Ribeiro <pedrorib25@gmail.com>
2021-12-03 15:54:47 +0000

    Completed challenge 2, day 2

---
+++ day2/challenge2/main.py
@@ -0,0 +1,23 @@
+def main():
+    print(calc())
+
+
+def calc():
+    hor_pos = 0
+    final_depth = 0
+    aim = 0
+    with open("../input.txt", "r") as f:
+        for line in f.readlines():
+            line_split = line.removesuffix("\n").split(" ")
+            val = int(line_split[1])
+            if line_split[0] == "forward":
+                hor_pos += val
+                final_depth += aim * val
+            elif line_split[0] == "down":
+                aim += val
+            elif line_split[0] == "up":
+                aim -= val
+    return hor_pos * final_depth
+
+if __name__ == "__main__":
+    main()
