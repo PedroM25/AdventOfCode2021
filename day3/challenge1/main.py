fdb291539207f08062b499e88492bbdfad6969c6
Pedro Ribeiro <pedrorib25@gmail.com>
2021-12-03 20:06:05 +0000

    Day 3 folder structure + challenge 1

---
+++ day3/challenge1/main.py
@@ -0,0 +1,31 @@
+def main():
+
+    with open("../input.txt", "r") as f:
+        num_columns = len((f.readline()).removesuffix("\n"))
+
+    with open("../input.txt", "r") as f:
+        zeros = [0] * num_columns
+        ones = [0] * num_columns
+
+        for line in f:
+            for i in range(num_columns):
+                if line[i] == "0":
+                    zeros[i] += 1
+                elif line[i] == "1":
+                    ones[i] += 1
+
+        gamma_rate = [""] * num_columns
+        epsilon_rate = [""] * num_columns
+        for i in range(num_columns):
+            if zeros[i] >= ones[i]:
+                gamma_rate[i] = "0"
+                epsilon_rate[i] = "1"
+            else:
+                gamma_rate[i] = "1"
+                epsilon_rate[i] = "0"
+
+        # power consumption
+        return int("".join(gamma_rate), 2) * int("".join(epsilon_rate), 2)
+
+if __name__ == "__main__":
+    print(main())
