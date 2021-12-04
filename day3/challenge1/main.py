0cdfb5a8e9d2cc6e60384ef88422405116b105e3
Pedro Ribeiro <pedrorib25@gmail.com>
2021-12-04 01:42:41 +0000

    Improved challenge1 readability

--- day3/challenge1/main.py
+++ day3/challenge1/main.py
@@ -1,13 +1,12 @@
 def main():
-
     with open("../input.txt", "r") as f:
-        num_columns = len((f.readline()).removesuffix("\n"))
+        all_lines = [line1.removesuffix("\n") for line1 in f.readlines()]
+        num_columns = len(all_lines[0])
 
-    with open("../input.txt", "r") as f:
         zeros = [0] * num_columns
         ones = [0] * num_columns
 
-        for line in f:
+        for line in all_lines:
             for i in range(num_columns):
                 if line[i] == "0":
                     zeros[i] += 1
@@ -26,6 +25,7 @@ def main():
 
         # power consumption
         return int("".join(gamma_rate), 2) * int("".join(epsilon_rate), 2)
+
 
 if __name__ == "__main__":
     print(main())
